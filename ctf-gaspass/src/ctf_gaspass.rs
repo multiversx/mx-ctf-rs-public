87fe85fec7d2c7316976b6fa35d26c62cee72c61
Andrei Marinica <andrei.marinica@multiversx.com>
2024-07-17 16:22:06 +0300

    gaspass fix, new addresses

--- ctf-gaspass/src/ctf_gaspass.rs
+++ ctf-gaspass/src/ctf_gaspass.rs
@@ -14,8 +14,10 @@ pub trait CtfGaspass: bump_common::BumpCommon {
     #[endpoint]
     fn gaspass(&self) -> bool {
         let gas_left = self.blockchain().get_gas_left();
-        let passed = gas_left == 500_000;
         let caller = self.blockchain().get_caller();
+        let the_key = self.the_key(&caller);
+        let passed = gas_left == the_key;
+        
         if passed {
             self.perform_bump(&caller);
         } else {
@@ -24,6 +26,11 @@ pub trait CtfGaspass: bump_common::BumpCommon {
         };
         self.gaspass_event(&caller, passed);
         passed
+    }
+
+    fn the_key(&self, caller: &ManagedAddress) -> u64 {
+        let bytes = caller.to_byte_array();
+        bytes.iter().map(|&b| b as u64).sum()
     }
 
     #[event("gaspass")]
