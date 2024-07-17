f0a581eeda932176d6c63eb7462e8f5d5e0c1f29
Andrei Marinica <andrei.marinica@multiversx.com>
2024-07-18 00:09:26 +0300

    gaspass fix

--- ctf-gaspass/src/ctf_gaspass.rs
+++ ctf-gaspass/src/ctf_gaspass.rs
@@ -3,6 +3,8 @@
 #[allow(unused_imports)]
 use multiversx_sc::imports::*;
 
+const KEY_BASELINE: u64 = 3_000_000;
+
 #[multiversx_sc::contract]
 pub trait CtfGaspass: bump_common::BumpCommon {
     #[init]
@@ -15,20 +17,19 @@ pub trait CtfGaspass: bump_common::BumpCommon {
     fn gaspass(&self) -> bool {
         let gas_left = self.blockchain().get_gas_left();
         let caller = self.blockchain().get_caller();
-        let the_key = self.the_key(&caller);
+        let the_key = KEY_BASELINE + self.personal_key(&caller);
         let passed = gas_left == the_key;
-        
+
         if passed {
             self.perform_bump(&caller);
         } else {
-            let caller = self.blockchain().get_caller();
             self.bumps(&caller).clear();
         };
         self.gaspass_event(&caller, passed);
         passed
     }
 
-    fn the_key(&self, caller: &ManagedAddress) -> u64 {
+    fn personal_key(&self, caller: &ManagedAddress) -> u64 {
         let bytes = caller.to_byte_array();
         bytes.iter().map(|&b| b as u64).sum()
     }
